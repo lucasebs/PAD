c06dedd4ec4ec7d2f684fbfae3ecfc52a7cdd2c3
lucasebs <lucasebatista@gmail.com>
2018-10-10 20:50:26 -0300

    Comment original codes to test a main

--- src/exemploimagens/Principal.java
+++ src/exemploimagens/Principal.java
@@ -9,11 +9,15 @@ import java.awt.image.BufferedImage;
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Semaphore;
 import javax.imageio.ImageIO;
+
+
 
 /**
  *
  * @author ruangomes
+ *
  */
 public class Principal {
     /**
@@ -21,32 +25,47 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ProcessadorImagens proc = new ProcessadorImagens();
-        try {
-            File f = new File("imagem_exemplo.jpg"); //image file path
-            System.out.println(f.getCanonicalPath());
-            BufferedImage img = ImageIO.read(f);
 
-            System.out.println("Imagem lida...");
+        Semaphore livre = new Semaphore(100);
+        Semaphore ocupado = new Semaphore(0);
+        Buffer buffer = new Buffer();
 
-            BufferedImage img_out = proc.brilho(img, -100);
-            System.out.println("Imagem processada...");
+        Produtor p = new Produtor(buffer,livre, ocupado);
+        Consumidor c = new Consumidor(buffer,livre,ocupado);
 
-            File f2 = new File("imagem_saida.jpg");  //output file path
-            ImageIO.write(img_out, "jpg", f2);
-            System.out.println("Imagem salva...");
+        Thread tp = new Thread(p);
+        Thread tc = new Thread(c);
 
-            BufferedImage img_out_lim = new BufferedImage(img.getWidth(), img.getHeight(), TYPE_INT_RGB);
+        tp.start();
+        tc.start();
 
-            proc.brilho_intervalo(img, 100, 2000, img_out_lim);
-            System.out.println("Imagem 2 processada...");
 
-            File f3 = new File("imagem_saida_limite.jpg");  //output file path
-            ImageIO.write(img_out_lim, "jpg", f3);
-            System.out.println("Imagem 2 salva...");
-
-        } catch (IOException e) {
-            System.out.println("Erro: " + e);
-        }
+//        ProcessadorImagens proc = new ProcessadorImagens();
+//        try {
+//            File f = new File("imagem_exemplo.jpg"); //image file path
+//            System.out.println(f.getCanonicalPath());
+//            BufferedImage img = ImageIO.read(f);
+//
+//            System.out.println("Imagem lida...");
+//
+//            BufferedImage img_out = proc.brilho(img, -100);
+//            System.out.println("Imagem processada...");
+//
+//            File f2 = new File("imagem_saida.jpg");  //output file path
+//            ImageIO.write(img_out, "jpg", f2);
+//            System.out.println("Imagem salva...");
+//
+//            BufferedImage img_out_lim = new BufferedImage(img.getWidth(), img.getHeight(), TYPE_INT_RGB);
+//
+//            proc.brilho_intervalo(img, 100, 2000, img_out_lim);
+//            System.out.println("Imagem 2 processada...");
+//
+//            File f3 = new File("imagem_saida_limite.jpg");  //output file path
+//            ImageIO.write(img_out_lim, "jpg", f3);
+//            System.out.println("Imagem 2 salva...");
+//
+//        } catch (IOException e) {
+//            System.out.println("Erro: " + e);
+//        }
     }
 }
